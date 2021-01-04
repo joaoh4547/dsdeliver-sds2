3f9fb20c87dd996b5d237935a0444ce807d8a320
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2021-01-04 14:33:47 -0300

    GET /orders

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		return orderRepository.findOrdersWithProducts().stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
